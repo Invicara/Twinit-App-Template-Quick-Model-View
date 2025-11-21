0a662d91dd42bb7c63c9c87ed5fee4d9a0c03814
scottmollon-invicara <scott.mollon@invicara.com>
2025-11-21 12:24:53 -0500

    Removing duplicate code

--- app/ipaCore/pageComponents/simpleViewer/consts.js
+++
@@ -1,5 +0,0 @@
-// model element property srcTypes considered to be numbers for filters
-export const numValueTypes = [ 'DOUBLE', 'FLOAT', 'INTEGER', 'LONG' ]
-
-// model element property srcTypes considered to be strings for filters
-export const stringValueTypes = [ 'STRING' ]
